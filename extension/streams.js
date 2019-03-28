87cd704961f64687981c1edc4b262d3f5eb5edc5
Florian Hammerschmitt <floha2011@hotmail.de>
2019-03-28 19:50:05 +0100

    twitch player now supports more than 4 streams, whenever that will be needed

--- extension/streams.js
+++ extension/streams.js
@@ -1,12 +1,14 @@
 var nodecg = require('./utils/nodecg-api-context').get();
 
-var streamsReplicant = nodecg.Replicant('twitch-streams', {'persistent':false,'defaultValue':[
+/*var streamsReplicant = nodecg.Replicant('twitch-streams', {'persistent':false,'defaultValue':[
 	{'channel':'speedrunslive','widthPercent':100,'heightPercent':100,'topPercent':0,'leftPercent':0,'quality':'chunked','volume':0.5,'paused':false,'hidden':true},
 	{'channel':'speedrunslive','widthPercent':100,'heightPercent':100,'topPercent':0,'leftPercent':0,'quality':'chunked','volume':0.5,'paused':false,'hidden':true},
 	{'channel':'speedrunslive','widthPercent':100,'heightPercent':100,'topPercent':0,'leftPercent':0,'quality':'chunked','volume':0.5,'paused':false,'hidden':true},
 	{'channel':'speedrunslive','widthPercent':100,'heightPercent':100,'topPercent':0,'leftPercent':0,'quality':'chunked','volume':0.5,'paused':false,'hidden':true},
-]});
+]});*/
 var runDataActiveRunReplicant = nodecg.Replicant("runDataActiveRun", 'nodecg-speedcontrol');
+
+var streamsReplicant = nodecg.Replicant('twitch-streams', {'persistent':false, 'defaultValue':[]});
 
 runDataActiveRunReplicant.on('change', newVal => {
 	if (!newVal) return;
@@ -15,6 +17,7 @@ runDataActiveRunReplicant.on('change', newVal => {
 	var index = 0;
 	newVal.teams.forEach(team => {
 		team.players.forEach(player => {
+                    streamsReplicant.value[index] = {'channel':'speedrunslive','widthPercent':100,'heightPercent':100,'topPercent':0,'leftPercent':0,'quality':'chunked','volume':0.5,'paused':false,'hidden':true};
 			if (!player.social || !player.social.twitch) {
 				nodecg.log.error('Twitch name for player '+index+' missing!');
 				streamsReplicant.value[index].paused = true;

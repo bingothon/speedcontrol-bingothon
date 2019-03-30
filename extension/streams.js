582904ea2cc86fa397c5f6a6ee75c4c5801ecd62
oliver <o-lohr@gmx.de>
2019-03-30 17:53:21 +0100

    Refactored streams a bit so all streams are changed at once

--- extension/streams.js
+++ extension/streams.js
@@ -8,32 +8,31 @@ var nodecg = require('./utils/nodecg-api-context').get();
 ]});*/
 var runDataActiveRunReplicant = nodecg.Replicant("runDataActiveRun", 'nodecg-speedcontrol');
 
-var streamsReplicant = nodecg.Replicant('twitch-streams', {'persistent':false, 'defaultValue':[]});
+var streamsReplicant = nodecg.Replicant('twitch-streams', {'defaultValue':[]});
 
 runDataActiveRunReplicant.on('change', newVal => {
 	if (!newVal) return;
 	
 	// grab all runners
-	var index = 0;
+	var newStreams = []
 	newVal.teams.forEach(team => {
 		team.players.forEach(player => {
-                    streamsReplicant.value[index] = {'channel':'speedrunslive','widthPercent':100,'heightPercent':100,'topPercent':0,'leftPercent':0,'quality':'chunked','volume':0.5,'paused':false,'hidden':true};
+			var current = {'channel':'speedrunslive','widthPercent':100,'heightPercent':100,'topPercent':0,'leftPercent':0,'quality':'chunked','volume':0.5,'paused':false,'hidden':true};
 			if (!player.social || !player.social.twitch) {
-				nodecg.log.error('Twitch name for player '+index+' missing!');
-				streamsReplicant.value[index].paused = true;
-				streamsReplicant.value[index].hidden = true;
+				nodecg.log.error(`Twitch name for player ${player.name} missing!`);
+				current.paused = true;
+				current.hidden = true;
 			} else {
-				streamsReplicant.value[index].channel = player.social.twitch;
-				streamsReplicant.value[index].hidden = false;
+				current.channel = player.social.twitch;
+				current.hidden = false;
 			}
-			index++;
+			newStreams.push(current);
 		});
 	});
-	index++;
 	// hide/mute/stop all other streams
-	while (index < 4) {
-		streamsReplicant.value[index].paused = true;
-		streamsReplicant.value[index].hidden = true;
-		index++;
+	// TODO refactor this to be dynamic in the layout
+	for (var i = newStreams.length;i<4;i++) {
+		newStreams.push({"paused":true, "hidden":true});
 	}
+	streamsReplicant.value = newStreams;
 });

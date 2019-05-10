349eba5efb5f19461aaccc99fde1675654a9b386
oliver <o-lohr@gmx.de>
2019-05-10 17:21:22 +0200

    Commit the definition as well

---
+++ extension/bingodefinitions.js
@@ -0,0 +1,8 @@
+module.exports = {
+    single:"In single bingo you need to complete a row/column/diagonal with 5 goals to finish",
+    double:"In double bingo you need to complete 2 rows/columns/diagonals with 5 goals each to finish, they may overlap",
+    triple:"In triple bingo you need to complete 3 rows/columns/diagonals with 5 goals each to finish, they may overlap",
+    lockout:"In lockout bingo each goal can only be claimed by the one player clicking it first, the first player locking up the majority of the goals (13) wins",
+    blackout:"In blackout bingo every goal on the board has to be done",
+    coop:", the players of a team share the same color on the board and work together",
+}

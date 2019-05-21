36ae1d44ca0e2a77cc7e928b127b31b1e00bc2f3
oliver <o-lohr@gmx.de>
2019-05-21 03:37:46 +0200

    Added missing bingodefinitions

--- extension/bingodefinitions.js
+++ extension/bingodefinitions.js
@@ -4,5 +4,7 @@ module.exports = {
     triple:"In triple bingo you need to complete 3 rows/columns/diagonals with 5 goals each to finish, they may overlap",
     lockout:"In lockout bingo each goal can only be claimed by the one player clicking it first, the first player locking up the majority of the goals (13) wins",
     blackout:"In blackout bingo every goal on the board has to be done",
+    "lockout invasion":"In lockout invasion bingo the players start marking goals on opposite sides and try to progress toward the opponents side. A goal can only be marked by one player and they can only mark as much goals as they have on the previous row/column on their side",
+    cinco:"In cinco bingo the players have to get 5 rows/columns/diagonals with 5 goals each to finish, they may overlap",
     coop:", the players of a team share the same color on the board and work together",
 }
